39a1cc1a8e55afe609fbab2d5d6137f30a286e44
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-08-20 05:28:08 -0300

    Fix linter errors

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,7 @@
-# rubocop:disable all
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require_relative '../config/environment'
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
@@ -46,8 +45,8 @@ RSpec.configure do |config|
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
   # `post` in specs under `spec/controllers`.
-  # You can disable this behaviour by removing the line
-  # below, and instead
+  #
+  # You can disable this behaviour by removing the line below, and instead
   # explicitly tag your specs with their type, e.g.:
   #
   #     RSpec.describe UsersController, type: :controller do
@@ -63,4 +62,3 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 end
-# rubocop:enable all
