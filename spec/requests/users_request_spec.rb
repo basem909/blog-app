56acac2e53503f1efe5b625c242884f7a8deb741
basem909 <basemshams30@gmail.com>
2022-08-24 16:07:55 -0300

    Add users_request_spec.rb test

--- spec/requests/users_request_spec.rb
+++ spec/requests/users_request_spec.rb
@@ -15,4 +15,19 @@ RSpec.describe 'Users', type: :request do
       expect(response.body).to include('Here will be the list of available users')
     end
   end
+
+  context 'Get /users/id' do
+    it 'Check if response status was correct' do
+      get '/users/id'
+      expect(response).to have_http_status(200)
+    end
+    it 'Check if a correct template was rendered.' do
+      get '/users/id'
+      expect(response).to render_template(:show)
+    end
+    it 'Check if the response body includes correct placeholder text.' do
+      get '/users/id'
+      expect(response.body).to include('Here will be the chosen user out of users list')
+    end
+  end
 end
