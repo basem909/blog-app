4f5ebafcb497b46492876a40f3e987bc52715c34
basem909 <basemshams30@gmail.com>
2022-08-24 16:02:24 -0300

    Add users-spec.rb test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'Get /users' do
+    it 'Check if response status was correct' do
+      get '/users'
+      expect(response).to have_http_status(200)
+    end
+    it 'Check if a correct template was rendered.' do
+      get '/users'
+      expect(response).to render_template(:index)
+    end
+    it 'Check if the response body includes correct placeholder text.' do
+      get '/users'
+      expect(response.body).to include('Here will be the list of available users')
+    end
   end
 end
