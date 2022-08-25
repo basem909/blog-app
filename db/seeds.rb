601b88785969d92abcbb24d2c461c1cd542bd351
basem909 <basemshams30@gmail.com>
2022-08-25 06:07:46 -0300

    Add data to the database to be applied in erb files

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,21 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+  first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.', posts_counter: 0)
+  second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Poland.', posts_counter: 0)
+ 
+
+  first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post',comments_counter: 0, likes_counter: 0)
+  second_post = Post.create(author: first_user, title: 'second not last', text: 'This is my second post',comments_counter: 0, likes_counter: 0)
+  third_post = Post.create(author: first_user, title: 'third is the best', text: 'This is my third post',comments_counter: 0, likes_counter: 0)
+  forth_post = Post.create(author: second_user, title: 'Hello', text: 'This is my first post',comments_counter: 0, likes_counter: 0)
+  fifth_post = Post.create(author: second_user, title: 'socializing', text: 'This platform is cool',comments_counter: 0, likes_counter: 0)
+
+  Comment.create(post: first_post, author: second_user, text: 'Hi Tom!' )
+  Comment.create(post: first_post, author: second_user, text: 'Nice to see you here' )
+  Comment.create(post: forth_post, author: first_user, text: 'Hi Lilly!' )
+  Comment.create(post: fifth_post, author: first_user, text: 'Same here' )
+  Comment.create(post: fifth_post, author: second_user, text: 'so you enjoy your time here' )
+  Comment.create(post: fifth_post, author: first_user, text: 'Same here' )
+
+  Like.create(post: first_post, author: second_user)
