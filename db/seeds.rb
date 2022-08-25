b1b54730b9fbbcf91cdf4488f8b3ebd89400d1c1
basem909 <basemshams30@gmail.com>
2022-08-25 17:07:11 -0300

    Add style to users  controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-first_user = User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.',
+first_user = User.create(name: 'Tom', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80', bio: 'Teacher from Mexico.',
                          posts_counter: 0)
-second_user = User.create(name: 'Lilly', photo: 'https://unsplash.com/photos/F_-0BxGuVvo',
+second_user = User.create(name: 'Lilly', photo: 'https://images.unsplash.com/photo-1508921912186-1d1a45ebb3c1?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
                           bio: 'Teacher from Poland.', posts_counter: 0)
 
 first_post = Post.create(author: first_user, title: 'Hello', text: 'This is my first post', comments_counter: 0,
@@ -31,3 +31,4 @@ Comment.create(post: fifth_post, author: second_user, text: 'so you enjoy your t
 Comment.create(post: fifth_post, author: first_user, text: 'Same here')
 
 Like.create(post: first_post, author: second_user)
+
